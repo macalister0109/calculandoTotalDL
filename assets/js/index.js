6b9928631bf9e7ea1ee80adff02eab5650e608a1
Macarena Jamett <82476477+macalister0109@users.noreply.github.com>
2024-02-24 22:41:23 -0300

    act

---
+++ assets/js/index.js
@@ -0,0 +1,25 @@
+precio = 400000;
+precioSpan = document.querySelector(".precio-inicial");
+precioSpan.innerHTML = precio;
+function adicionarElemento(){
+    cantidad = document.querySelector('.cantidad');
+    valorCantidad = Number(cantidad.innerHTML);
+    valorCantidad++;
+    cantidad.innerHTML = valorCantidad;
+}
+
+function restarElemento(){
+    cantidad = document.querySelector('.cantidad');
+    valorCantidad = Number(cantidad.innerHTML);
+    valorCantidad--;
+    cantidad.innerHTML = valorCantidad;
+}
+
+function calcularPrecio(){
+    cantidad = document.querySelector('.cantidad');
+    valorCantidad = Number(cantidad.innerHTML);
+    precioFinal = precio * valorCantidad;
+    valorTotal = document.querySelector('.valor-total');
+    valorTotal.innerHTML = precioFinal;
+    
+}
